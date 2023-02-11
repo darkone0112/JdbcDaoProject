c91730e43ff8d7cb08688dc2875d6e788cf2af05
ziqiang <1213703963@qq.com>
2023-02-10 22:21:16 -0500

    funcionaTienda Pero no puede buscar

---
+++ ProyectoFinal/src/TiendaDAO/TiendaGallery.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TiendaDAO;
+
+/**
+ *
+ * @author osboxes
+ */
+public class TiendaGallery {
+     static TiendaInterface getTiendaDao() {
+           return new TiendaBean();
+    }
+
+    
+}
